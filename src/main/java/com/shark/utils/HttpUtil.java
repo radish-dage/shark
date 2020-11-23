6f096281dbe35a71037bce4db4ba5996a19f094c
dage <1192274716@qq.com>
2020-11-23 23:49:25 +0800

    方法封装

--- src/main/java/com/shark/utils/HttpUtil.java
+++ src/main/java/com/shark/utils/HttpUtil.java
@@ -1,6 +1,12 @@
 package com.shark.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.shark.common.HttpCode;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Iterator;
@@ -19,24 +25,55 @@ public class HttpUtil {
      * 获取http连接
      * @param urlPath 连接路径
      * @param requestMethod 请求方法
-     * @param reqHeaderPro 请求头属性，不定参数，有参时最多也只有一个
+     * @param reqHeader 请求头属性，不定参数，有参时最多也只有一个
      * @return
+     * @throws IOException
      */
-    public static HttpURLConnection createHttpUrlConn(String urlPath, String requestMethod, Map<String,String>...reqHeaderPro) throws IOException {
+    public static HttpURLConnection openHttpUrlConn(String urlPath, String requestMethod, Map<String,String>...reqHeader) throws IOException {
         URL url = new URL(urlPath);
+        //
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
+        //设置请求方法
         httpURLConnection.setRequestMethod(requestMethod);
 
         //设置请求头
-        if(reqHeaderPro[0].size() != 0){
-            Iterator<Map.Entry<String,String>> iterator = reqHeaderPro[0].entrySet().iterator();
+        if(reqHeader[0].size() != 0){
+            Iterator<Map.Entry<String,String>> iterator = reqHeader[0].entrySet().iterator();
             while(iterator.hasNext()){
                 Map.Entry<String,String> entry = iterator.next();
                 httpURLConnection.setRequestProperty(entry.getKey(), entry.getValue());
             }
         }
 
+        //打卡到此url引用的资源的通信连接，已打开情况下忽略该调用
+        httpURLConnection.connect();
+
         return httpURLConnection;
+    }
+
+    /**
+     * 获取数据缓冲流
+     * @param httpURLConnection
+     * @return
+     * @throws IOException
+     */
+    public static BufferedReader getHttpUrlBufferedReader(HttpURLConnection httpURLConnection) throws IOException {
+        return new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), HttpCode.UTF8));
+    }
+
+    /**
+     * 获取流中json数据
+     * @param br
+     * @return
+     * @throws IOException
+     */
+    public static JSONObject getBrJsonData(BufferedReader br) throws IOException {
+        String lineString;
+        StringBuilder sb = new StringBuilder();
+        while( (lineString = br.readLine()) != null) {
+            sb.append(lineString);
+        }
+        return JSON.parseObject(sb.toString());
     }
 }
