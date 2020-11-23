286c645c507ebbd0a565c0cb5f4c05e632eb1c6f
dage <1192274716@qq.com>
2020-11-23 23:50:07 +0800

    中央控制器，动态设参，返回json

--- src/main/java/com/shark/servlet/DispatcherServlet.java
+++ src/main/java/com/shark/servlet/DispatcherServlet.java
@@ -11,9 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Parameter;
+import java.util.*;
 
 /**
  * 中央控制器，处理所有请求到指定方法的映射
@@ -42,10 +41,11 @@ public class DispatcherServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("GET Request");
         requestUrlToMethodInvoke(req, resp, getMap);
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("POST Request");
         requestUrlToMethodInvoke(req, resp, postMap);
     }
@@ -53,16 +53,39 @@ public class DispatcherServlet extends HttpServlet{
     private void requestUrlToMethodInvoke(HttpServletRequest req, HttpServletResponse resp, Map<String, Method> map) {
         //1.获取请求的路径
         String reqUrl = req.getRequestURI();
-        System.out.println("请求路径："+reqUrl);
+        String[] reqUrls = reqUrl.split("/");
+        reqUrl = "/" + reqUrls[reqUrls.length - 1].substring(0,reqUrls[reqUrls.length - 1].length() - 3);
+
+        //获取参数列表
+        Map<String,String[]> paramMap = req.getParameterMap();
 
         //2.根据请求路径从map中取出对应的method然后回调
         Method method = map.get(reqUrl);
-        if(method != null){
+
+        if(method == null){//获取不到对应的映射方法，转发到404
+            try {
+                req.getRequestDispatcher("/html/404.html").forward(req,resp);
+            } catch (ServletException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+
+            //动态设参
+            Parameter[] parameters = method.getParameters();
+            Object[] params = new Object[parameters.length];
+            for (int i = 0; i < parameters.length; i++) {
+                String[] paramsStr = paramMap.get(parameters[i].getName());
+                params[i] = paramsStr[0];
+            }
+
             try {
                 //实例化
                 Object object = method.getDeclaringClass().getDeclaredConstructor().newInstance();
-                //回调方法
-                method.invoke(object,req,resp);
+                //回调方法，以json格式响应返回值
+                String json = (String) method.invoke(object,params);
+                resp.getWriter().write(json);
             } catch (InstantiationException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
@@ -71,16 +94,11 @@ public class DispatcherServlet extends HttpServlet{
                 e.printStackTrace();
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
-            }
-        }else{
-            try {
-                resp.getWriter().write("request have error,please check url");
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
-
-
     }
 
     /**
@@ -98,11 +116,10 @@ public class DispatcherServlet extends HttpServlet{
                 for (Method method : meThods) {
                     //过滤不存在注解的方法
                     if(method.isAnnotationPresent(RequestUrlMapping.class)){
+                        //获取有注解方法的value值，对应我们的请求url，缓存到map集合
                         if(HttpCode.GET.equals(method.getAnnotation(RequestUrlMapping.class).type())){
-                            //获取有注解方法的value值，对应我们的请求url，缓存到map集合
                             getMap.put(method.getAnnotation(RequestUrlMapping.class).url(), method);
                         }else if(HttpCode.POST.equals(method.getAnnotation(RequestUrlMapping.class).type())){
-                            //获取有注解方法的value值，对应我们的请求url，缓存到map集合
                             postMap.put(method.getAnnotation(RequestUrlMapping.class).url(), method);
                         }
                     }
